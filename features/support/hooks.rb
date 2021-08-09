029e8bd1307f120c6bcf10b6925cc52bf3495e94
viniciuscarneironascimento <contato.vinicius@gmail.com>
2021-08-09 17:10:22 -0300

    Adicionando evidências: usando gancho "AFTER" CHAMADA DO RELATÓRIO ALLURE; Depois de cada execução digita no prompt "allure serve logs\"

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,3 +9,15 @@ Before do
   # page.driver.browser.manage.window.maximize
   page.current_window.resize_to(1440, 900)
 end
+
+After do
+  # Depois (After) da execução do último step de um cenário, ou seja, no step "ENTÃO", obtenho um print do resultado com o comando abaixo. Este print será salvo com a extensão ".png" conforme definido, em seguida guardo na pasta LOGS "logs/tem_screenshot.png". Salvo o print desta evidência em uma variável.
+  temp_shot = page.save_screenshot("logs/tem_screenshot.png")
+
+  # Anexo o print desta evidência no relatório Allure atrvés dos comandos abaixo
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
+end
