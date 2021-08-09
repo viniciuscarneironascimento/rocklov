a19fea9707c4be9a6055dec6687f932a7733c1b2
viniciuscarneironascimento <contato.vinicius@gmail.com>
2021-08-09 11:32:04 -0300

    ALLURE REPORT: Baixando, configurando, guardando informações na pasta logs

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@
 
 source "https://rubygems.org"
 
+gem "oj", "3.12.2"
+gem "allure-cucumber", "2.14.1"
 gem "cucumber", "5.2"
 gem "capybara", "3.33"
 gem "selenium-webdriver", "3.142.7"
 gem "rspec", "3.10"
 gem "faker", "2.14"
 gem "mongo", "2.13.1"
-gem "pry"
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,4 @@
+require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"
 require "faker"
@@ -10,4 +11,12 @@ Capybara.configure do |config|
   config.app_host = "http://rocklov-web:3000"
   # Com a nova versão, o sistema está demorando para processar e o Capybara não está encontrando o elemento na tela. Desta forma irei adicionar uma espera default no arquivo env.rb. A espera padrão do Capybara são 2 segundos.
   config.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+  # Guardar o relatório no diretório pasta logs
+  config.results_directory = "/logs"
+
+  # Limpa e atualiza o relatório com a última execução
+  config.clean_results_directory = true
 end
