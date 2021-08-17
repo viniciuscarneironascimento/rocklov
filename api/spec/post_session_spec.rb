e1f80a1dc417f1d9c2a7b812a4e60bf1dee2c588
viniciuscarneironascimento <contato.vinicius@gmail.com>
2021-08-16 23:54:15 -0300

    Programação orientada a testes

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -57,9 +57,15 @@ describe "POST/sessions" do
 
       # OBSERVAÇÃO: foi incluído no topo do código "require_relative "routes/sessions", ou seja, para fazer a chamada da classe "Sessions".
       # O BEFORE vai ficar agora assim:
-      @result = Sessions.new.login("betao@yahoo.com.br", "12345")
-    end
+      #   @result = Sessions.new.login("betao@yahoo.com.br", "12345")
 
+      # Com a implementação da linha abaixo, a linha acima será comentada
+      payload = { email: "betao@yahoo.com.br", password: "12345" }
+
+      # Com a implementação da linha acima, a linha abaixo ficará:
+      @result = Sessions.new.login(payload)
+    end
+    # --------------------------------------------------------------------------------------------------
     # Cada "IT" abaixo é um teste independente
     # Neste eu valido o status code
     it "valida status code" do
@@ -71,19 +77,102 @@ describe "POST/sessions" do
       expect(@result.parsed_response["_id"].length).to eql 24
     end
   end
+  # --------------------------------------------------------------------------------------------------
 
   # Copiando todo o contexto acima para criar novo teste. Neste cenário coloco senha inválida
-  context "senha invalida" do
-    before (:all) do
-      @result = Sessions.new.login("betao@yahoo.com.br", "errada1234")
-    end
+  #   context "senha invalida" do
+  #     before (:all) do
+  #       # A linha abaixo será substituída pelas linhasa seguir:
+  #       #   @result = Sessions.new.login("betao@yahoo.com.br", "errada1234")
+  #       payload = { email: "betao@yahoo.com.br", password: "errada1234" }
+  #       @result = Sessions.new.login(payload)
+  #     end
 
-    it "valida status code" do
-      expect(@result.code).to eql 401
-    end
+  #     it "valida status code" do
+  #       expect(@result.code).to eql 401
+  #     end
 
-    it "valida mensagem" do
-      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+  #     it "valida mensagem" do
+  #       expect(@result.parsed_response["error"]).to eql "Unauthorized"
+  #     end
+  #   end
+
+  # Copiando todo o contexto acima para criar novo teste. Neste cenário coloco senha inválida
+  #   context "usuario nao existe" do
+  #     before (:all) do
+  #       # A linha abaixo será substituída pelas linhasa seguir:
+  #       #   @result = Sessions.new.login("betao@yahoo.com.br", "errada1234")
+  #       payload = { email: "404@yahoo.com.br", password: "errada1234" }
+  #       @result = Sessions.new.login(payload)
+  #     end
+
+  #     it "valida status code" do
+  #       expect(@result.code).to eql 401
+  #     end
+
+  #     it "valida mensagem" do
+  #       expect(@result.parsed_response["error"]).to eql "Unauthorized"
+  #     end
+  #   end
+  #   --------------------------------------------------------------------------------------------------
+  # Nova implementação otimizando o código e substituindo os CONTEXTOS
+  # Criamos um ARRAY de HASH contendo os possíveis cenários de teste
+  examples = [
+    {
+      title: "senha incorreta",
+      payload: { email: "betao@yahoo.com.br", password: "errada1234" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "usuario nao existe",
+      payload: { email: "404@yahoo.com.br", password: "errada1234" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "email em branco",
+      payload: { email: "", password: "12345" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "sem campo email",
+      payload: { password: "12345" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "senha em branco",
+      payload: { email: "betao@yahoo.com.br", password: "" },
+      code: 412,
+      error: "required password",
+    },
+    {
+      title: "sem campo senha",
+      payload: { email: "betao@yahoo.com.br" },
+      code: 412,
+      error: "required password",
+    },
+  ]
+
+  # Como a variável "examples" é um ARRAY, farei um "for each" para percorrer cada elemento
+  # O argumento "e" é o índice do array
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before (:all) do
+        # A linha abaixo será substituída pelas linhasa seguir:
+        #   @result = Sessions.new.login("betao@yahoo.com.br", "errada1234")
+        @result = Sessions.new.login(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida mensagem" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
--- api/spec/routes/sessions.rb
+++ api/spec/routes/sessions.rb
@@ -8,9 +8,12 @@ class Sessions
   # Inclusão da linha abaixo que é uma propiedade do HTTParty que recebe a URL da API. Desta forma, a rota "http://rocklov-api:3333/sessions" utilizada no post linha 20 será substituída apenas poe "/sessions"
   base_uri "http://rocklov-api:3333"
 
-  def login(email, pass)
-    # Monto o payload em uma variável
-    payload = { email: email, password: pass }
+  # O meétodo abaixo será refatorado e receberá um objeto "payload" inteiro, ou seja, de "def login(email, pass)" para:
+  #   def login(email, pass)
+  def login(payload)
+    # Monto o payload em uma variável.
+    # Devido a alteração na passagem de parâmetro do metódo acima,, a linha abaixo passará a ser usada no script de teste, ou seja, no arquivo "post_session_spec.rb"
+    # payload = { email: email, password: pass }
 
     # Realizanao POST: passa a URL, o corpo o payload no formato JSON e o cabeçalho
     #   @result = HTTParty.post(
